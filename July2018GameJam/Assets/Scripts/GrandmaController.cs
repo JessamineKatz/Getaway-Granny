9d516bc833837847e284e83682c3ebc0c5f071d4
Sam Katz <sammykatz@sbcglobal.net>
2018-07-16 21:13:09 -0500

    Basic player movement system.

---
+++ July2018GameJam/Assets/Scripts/GrandmaController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrandmaController : MonoBehaviour
+{
+    private Rigidbody2D Rigidbody;
+
+    public float BaseSpeed = 5; //public variables are exposed in the Unity inspector.
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    float GetSpeed() //exists so it's easy to add movement slowing effect later.
+    {
+        return BaseSpeed; 
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+	    float xInput = 0;
+	    float yInput = 0;
+
+	    yInput = Input.GetAxisRaw("Vertical");
+	    xInput = Input.GetAxisRaw("Horizontal");
+
+        Rigidbody.velocity = new Vector2(xInput, yInput);
+    }
+}
