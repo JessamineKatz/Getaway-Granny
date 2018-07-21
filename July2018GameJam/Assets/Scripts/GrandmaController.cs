2511b3b1e9b7fcef63a53c44ebbc63626609e773
Sam Katz <sammykatz@sbcglobal.net>
2018-07-21 09:29:48 -0500

    Moved slipper spawning into the Grandma controller. Added slipper pickup object. Added new guard to the level. Updated pathfinding values in Guard prefab.

--- July2018GameJam/Assets/Scripts/GrandmaController.cs
+++ July2018GameJam/Assets/Scripts/GrandmaController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GrandmaController : MonoBehaviour
 {
@@ -9,6 +11,11 @@ public class GrandmaController : MonoBehaviour
     public float BaseSpeed = 5; //public variables are exposed in the Unity inspector.
 
     private static GameObject granny;
+
+    public GameObject sliperPrefab;
+    public Text slipperCountText;
+
+    public int slipperCount = 0;
 
     public static GameObject GetInstance()
     {
@@ -27,9 +34,8 @@ public class GrandmaController : MonoBehaviour
         return BaseSpeed; 
     }
 
-
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 	    float xInput = 0;
 	    float yInput = 0;
 
@@ -37,5 +43,21 @@ public class GrandmaController : MonoBehaviour
 	    xInput = Input.GetAxisRaw("Horizontal");
 
         Rigidbody.velocity = new Vector2(xInput, yInput);
+
+
+        if (Input.GetButtonDown("Fire1") && slipperCount > 0)
+        {
+            Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+            Instantiate(sliperPrefab, new Vector3(p.x, p.y, 0.0f), Quaternion.identity);
+            slipperCount--;
+        }
+        else if (Input.GetKeyDown(KeyCode.F12))
+        {
+            slipperCount++;
+        }
+
+        slipperCountText.GetComponent<Text>().text = "Slipper count: " + slipperCount;
     }
+
+    
 }
---
+++ July2018GameJam/Assets/Scripts/SlipperPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlipperPickup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GrandmaController granny = other.GetComponent<GrandmaController>();
+
+        if (granny != null)
+        {
+            Destroy(this.gameObject);
+            granny.slipperCount++;
+        }
+    }
+
+}
