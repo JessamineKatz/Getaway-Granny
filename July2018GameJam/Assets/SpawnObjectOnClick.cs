73b0110ed0f0c7aa7be56677f84def2c43be5eaa
Tyler Davis <tmd33196@gmail.com>
2018-07-20 19:40:37 -0500

    added guard distraction on click

---
+++ July2018GameJam/Assets/SpawnObjectOnClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectOnClick : MonoBehaviour {
+
+    public GameObject prefab;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+        {
+            Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+            Instantiate(prefab, new Vector3(p.x, p.y, 0.0f), Quaternion.identity);
+        }
+	}
+}
